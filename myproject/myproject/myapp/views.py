ac04113ab3fd75d1784959d4ed59e09b6aea568d
samalviya <support102@anaxee.com>
2024-06-15 14:33:55 +0530

    yes

---
+++ myproject/myproject/myapp/views.py
@@ -0,0 +1,173 @@
+from django.shortcuts import render
+from .utils import find_smallest_number, find_largest_number, calculate_sum
+import os
+import xml.dom.minidom
+import xml.etree.ElementTree as ET
+from django.http import HttpResponse
+from .kml_script import parse_coordinates1, parse_kml1,merge_segments, create_boundary_kml, create_kml, parse_coordinates, create_kmal, buffer_and_create_polygons, format_kml_coordinates
+import csv
+
+def home(request):
+    return render(request, 'home.html')
+
+def smallest_number_view(request):
+    output_data = None
+    if request.method == 'POST':
+        input_data = request.POST.get('input_data')
+        try:
+            numbers = list(map(int, input_data.split(',')))
+            output_data = find_smallest_number(numbers)
+        except ValueError:
+            output_data = 'Invalid input. Please enter a comma-separated list of numbers.'
+    return render(request, 'smallest_number.html', {'output_data': output_data})
+
+def largest_number_view(request):
+    output_data = None
+    if request.method == 'POST':
+        input_data = request.POST.get('input_data')
+        try:
+            numbers = list(map(int, input_data.split(',')))
+            output_data = find_largest_number(numbers)
+        except ValueError:
+            output_data = 'Invalid input. Please enter a comma-separated list of numbers.'
+    return render(request, 'largest_number.html', {'output_data': output_data})
+
+def sum_view(request):
+    output_data = None
+    if request.method == 'POST':
+        input_data = request.POST.get('input_data')
+        try:
+            numbers = list(map(int, input_data.split(',')))
+            output_data = calculate_sum(numbers)
+        except ValueError:
+            output_data = 'Invalid input. Please enter a comma-separated list of numbers.'
+    return render(request, 'sum.html', {'output_data': output_data})
+
+def kml_view(request):
+    result = None
+    if request.method == 'POST':
+        coordinates_str = request.POST.get('coordinates_str')
+        try:
+            segments = parse_coordinates1(coordinates_str)
+            output_file = 'output.kml'
+            create_kmal(segments, output_file)
+
+            tree = ET.parse(output_file)
+            root = tree.getroot()
+
+            segments = []
+            for placemark in root.findall('.//{http://www.opengis.net/kml/2.2}Placemark'):
+                name = placemark.find('{http://www.opengis.net/kml/2.2}name').text
+                coordinates = placemark.find('.//{http://www.opengis.net/kml/2.2}coordinates').text.strip()
+                parsed_coords = parse_coordinates(coordinates)
+                segments.append((name, parsed_coords))
+
+            create_kml(segments, output_file)
+
+            doc = xml.dom.minidom.parse(output_file)
+            coordinates_elements = doc.getElementsByTagName('coordinates')
+            if not coordinates_elements:
+                print("No coordinates found in the KML file.")
+            
+            coordinates_text = coordinates_elements[0].firstChild.nodeValue.strip()
+            coords_list = coordinates_text.split()
+            start_coords = coords_list[0]
+            end_coords = coords_list[-1]
+
+            if start_coords == end_coords:
+                print("Polygon")
+                coordinates = parse_kml1(output_file)
+                create_boundary_kml(coordinates, output_file)
+            else:
+                print("LineString")
+                buffer_and_create_polygons(output_file)
+
+            with open(output_file, 'r') as kml_file:
+                kml_data = kml_file.read()
+
+            result = format_kml_coordinates(output_file)
+       
+
+        except Exception as e:
+            result = f"An error occurred: {str(e)}"
+            
+
+
+    return render(request, 'kml.html', {'result': result})
+
+def download_kml(request):
+    kml_file_path = 'output.kml'
+    if os.path.exists(kml_file_path):
+        with open(kml_file_path, 'rb') as kml_file:
+            response = HttpResponse(kml_file.read(), content_type='application/vnd.google-earth.kml+xml')
+            response['Content-Disposition'] = 'attachment; filename="output.kml"'
+            return response
+    else:
+        return HttpResponse("File not found", status=404)
+
+
+def process_csv_view(request):
+    if request.method == 'POST' and request.FILES['csv_file']:
+        csv_file = request.FILES['csv_file']
+        if not csv_file.name.endswith('.csv'):
+            return HttpResponse('File is not CSV type')
+
+        # Ensure to open the CSV file in text mode and handle BOM
+        csv_data = csv.DictReader(csv_file.read().decode('utf-8-sig').splitlines())
+        
+        rows = list(csv_data)
+        for row in rows:
+            plot_id = row["Plot Id"]
+            coordinates_str = row["Final Polygon of farm"]
+
+            segments = parse_coordinates1(coordinates_str)
+            output_filename = f"{plot_id}.kml"
+            create_kmal(segments, output_filename)
+
+            tree = ET.parse(output_filename)
+            root = tree.getroot()
+
+            segments = []
+            for placemark in root.findall('.//{http://www.opengis.net/kml/2.2}Placemark'):
+                name = placemark.find('{http://www.opengis.net/kml/2.2}name').text
+                coordinates = placemark.find('.//{http://www.opengis.net/kml/2.2}coordinates').text.strip()
+                parsed_coords = parse_coordinates(coordinates)
+                segments.append((name, parsed_coords))
+
+            merged_segments = merge_segments(segments)
+            create_kml(merged_segments, output_filename)
+
+            doc = xml.dom.minidom.parse(output_filename)
+            coordinates_elements = doc.getElementsByTagName('coordinates')
+            if not coordinates_elements:
+                print(f"No coordinates found in the KML file for plot {plot_id}.")
+
+            coordinates_text = coordinates_elements[0].firstChild.nodeValue.strip()
+            coords_list = coordinates_text.split()
+            start_coords = coords_list[0]
+            end_coords = coords_list[-1]
+
+            if start_coords == end_coords:
+                print(f"Polygon for plot {plot_id}")
+                coordinates = parse_kml1(output_filename)
+                create_boundary_kml(coordinates, output_filename)
+            else:
+                print(f"LineString for plot {plot_id}")
+                buffer_and_create_polygons(output_filename)
+
+            formatted_coordinates = format_kml_coordinates(output_filename)
+            row["Formatted Coordinates"] = formatted_coordinates
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="processed_data.csv"'
+
+        # Write to the response CSV file
+        fieldnames = csv_data.fieldnames + ["Formatted Coordinates"]
+        csv_writer = csv.DictWriter(response, fieldnames=fieldnames)
+        csv_writer.writeheader()
+        csv_writer.writerows(rows)
+
+        return response
+
+    return render(request, 'kml.html')
+
